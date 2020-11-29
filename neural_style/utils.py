7c822b594c4801d8f7d9fb822984a7b1859d9ade
SOLLA-bitsensing <solla@bitsensing.com>
2020-11-29 19:50:56 +0900

    Add real time style transfer webcam baseline

--- neural_style/utils.py
+++ neural_style/utils.py
@@ -1,5 +1,7 @@
 import torch
+import cv2
 from PIL import Image
+from torchvision import transforms
 
 
 def load_image(filename, size=None, scale=None):
@@ -32,3 +34,62 @@ def normalize_batch(batch):
     std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
     batch = batch.div_(255.0)
     return (batch - mean) / std
+
+
+# Preprocessing ~ Image to Tensor
+def itot(img, max_size=None):
+    # Rescale the image
+    if (max_size==None):
+        itot_t = transforms.Compose([
+            #transforms.ToPILImage(),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.mul(255))
+        ])    
+    else:
+        H, W, C = img.shape
+        image_size = tuple([int((float(max_size) / max([H,W]))*x) for x in [H, W]])
+        itot_t = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(image_size),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x.mul(255))
+        ])
+
+    # Convert image to tensor
+    tensor = itot_t(img)
+
+    # Add the batch_size dimension
+    tensor = tensor.unsqueeze(dim=0)
+    return tensor
+
+# Preprocessing ~ Tensor to Image
+def ttoi(tensor):
+    # Add the means
+    #ttoi_t = transforms.Compose([
+    #    transforms.Normalize([-103.939, -116.779, -123.68],[1,1,1])])
+
+    # Remove the batch_size dimension
+    tensor = tensor.squeeze()
+    #img = ttoi_t(tensor)
+    img = tensor.cpu().numpy()
+    
+    # Transpose from [C, H, W] -> [H, W, C]
+    img = img.transpose(1, 2, 0)
+    return img
+
+def transfer_color(src, dest):
+    """
+    Transfer Color using YIQ colorspace. Useful in preserving colors in style transfer.
+    This method assumes inputs of shape [Height, Width, Channel] in BGR Color Space
+    """
+    src, dest = src.clip(0,255), dest.clip(0,255)
+        
+    # Resize src to dest's size
+    H,W,_ = src.shape 
+    dest = cv2.resize(dest, dsize=(W, H), interpolation=cv2.INTER_CUBIC)
+    
+    dest_gray = cv2.cvtColor(dest, cv2.COLOR_BGR2GRAY) #1 Extract the Destination's luminance
+    src_yiq = cv2.cvtColor(src, cv2.COLOR_BGR2YCrCb)   #2 Convert the Source from BGR to YIQ/YCbCr
+    src_yiq[...,0] = dest_gray                         #3 Combine Destination's luminance and Source's IQ/CbCr
+    
+    return cv2.cvtColor(src_yiq, cv2.COLOR_YCrCb2BGR).clip(0,255)  #4 Convert new image from YIQ back to BGR
